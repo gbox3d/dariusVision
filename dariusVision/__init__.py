04d621b5164f8eec160546c2f8f06125efb3526a
gbox3d <gbox3d@gmail.com>
2023-06-01 16:45:24 +0900

    0.0.4

--- dariusVision/__init__.py
+++ dariusVision/__init__.py
@@ -1,9 +1,19 @@
 # from .cam import lastest_Cam
-from .cam import lastest_Cam2 
-from .realsense2 import rs2_AsyncCam
+# from .cam import lastest_Cam2 
+# from .realsense2 import rs2_AsyncCam
 from .utils import *
 
-version='0.0.3'
+version='0.0.4'
 
-def get_version():
+def getVersion():
     return version
+
+def getCameraClass(type='rs2'):
+    if type=='rs2':
+        from .cam import rs2_AsyncCam 
+        return rs2_AsyncCam
+    elif type=='cv':
+        from .cam import lastest_Cam2
+        return lastest_Cam2
+    else:
+        return None
--- example/getFrame.py
+++ example/getFrame.py
@@ -1,18 +1,30 @@
 #%%
-from dariusVision import lastest_Cam2,get_version,rs2_AsyncCam
 import cv2 as cv
 
 from PIL import Image
 from IPython.display import display
 
-print(get_version())
+import os
+
+os.chdir("../")
+
+#%%
+from dariusVision import getCameraClass,getVersion
+
+print(getVersion())
+
+#%%
+lcam = getCameraClass(type='cv')(vid_src=0)
 # %%
+from dariusVision import rs2_AsyncCam
 lcam = rs2_AsyncCam()
+
 # %%
 lcam.startCamera()
+
 #%%
 _,frame = lcam.getFrame()
-# %%
 display(Image.fromarray( cv.cvtColor(frame,cv.COLOR_BGR2RGB) ))
 # %%
 lcam.stopCamera()
+# %%
